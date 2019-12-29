c808a165a56ca1c79cb2e8a54ee4af4eb3c5f8fe
Valentin Debon <valentin@debon.net>
2019-12-29 02:33:37 +0100

    New version

--- hash_table.c
+++ hash_table.c
@@ -1,146 +1,109 @@
-/*
-	hash_table.c
-	Copyright (c) 2019, Valentin Debon
-
-	This file is part of the hash_table repository
-	subject the BSD 3-Clause License, see LICENSE
-*/
 #include "hash_table.h"
 
 #include <stdlib.h>
 
-struct hash_table_bucket {
-	struct hash_table_bucket *next;
+#define HASH_TABLE_BUCKETOF(table, key) ((table)->buckets + (((table)->size - 1) & (key)))
+#define HASH_TABLE_BUCKET_DEFAULT 16
+
+struct hash_table_pair {
 	hash_table_value_t *value;
 	hash_table_key_t key;
 };
 
+struct hash_table_bucket {
+	unsigned long capacity;
+	unsigned long count;
+	struct hash_table_pair *pairs;
+};
+
 struct hash_table {
-	uint64_t length;
+	unsigned long size;
 	struct hash_table_bucket buckets[];
 };
 
-static struct hash_table_bucket *
-hash_table_bucket_create(hash_table_key_t key,
-	hash_table_value_t *value) {
-	struct hash_table_bucket *bucket = malloc(sizeof(*bucket));
-
-	bucket->next = NULL;
-	bucket->value = value;
-	bucket->key = key;
-
-	return bucket;
-}
-
-static void
-hash_table_bucket_destroy(struct hash_table_bucket *bucket) {
-
-	if(bucket->next != NULL) {
-		hash_table_bucket_destroy(bucket->next);
-	}
-
-	free(bucket);
-}
-
 struct hash_table *
-hash_table_create(uint64_t length) {
-	struct hash_table *table = NULL;
+hash_table_create(unsigned long power) {
+	const unsigned long size = 1 << power;
+	struct hash_table *table = calloc(1, sizeof(*table) + size * sizeof(*table->buckets));
 
-	if(length != 0
-		&& (table = calloc(sizeof(*table) + sizeof(struct hash_table_bucket[length]), 1)) != NULL) {
-		table->length = length;
-	}
+	table->size = size;
 
 	return table;
 }
 
 void
 hash_table_destroy(struct hash_table *table) {
-	struct hash_table_bucket *bucket = table->buckets,
-		* const end = table->buckets + table->length;
+	struct hash_table_bucket *current = table->buckets,
+		* const end = table->buckets + table->size;
 
-	while(bucket != end) {
-		if(bucket->next != NULL) {
-			hash_table_bucket_destroy(bucket->next);
-		}
-		++bucket;
+	while(current < end) {
+		free(current->pairs);
+		current++;
 	}
 
 	free(table);
 }
 
-void
-hash_table_insert(struct hash_table *table,
-	hash_table_key_t key, hash_table_value_t *value) {
-	struct hash_table_bucket *bucket = table->buckets + key % table->length;
+hash_table_value_t *
+hash_table_update(struct hash_table *table, hash_table_key_t key, hash_table_value_t *value) {
+	struct hash_table_bucket * const bucket = HASH_TABLE_BUCKETOF(table, key);
+	struct hash_table_pair *current = bucket->pairs,
+		* const end = bucket->pairs + bucket->count;
+	hash_table_value_t *updated;
 
-	if(value != NULL) {
-		if(bucket->value == NULL) {
-			bucket->value = value;
-			bucket->key = key;
-		} else {
-			while(bucket->key != key
-				&& bucket->next != NULL) {
-				bucket = bucket->next;
-			}
-	
-			if(bucket->key == key) {
-				bucket->value = value;
-			} else {
-				bucket->next = hash_table_bucket_create(key, value);
-			}
-		}
+	while(current < end && current->key != key) {
+		current++;
 	}
-}
 
-void
-hash_table_remove(struct hash_table *table,
-	hash_table_key_t key) {
-	struct hash_table_bucket *bucket = table->buckets + key % table->length;
-
-	if(bucket->key == key) {
-		if(bucket->next != NULL) {
-			struct hash_table_bucket *next = bucket->next;
-			*bucket = *next;
-			next->next = NULL;
-			hash_table_bucket_destroy(next);
-		} else {
-			bucket->value = NULL;
+	if(current == end) {
+		if(bucket->count == bucket->capacity) {
+			bucket->capacity = bucket->capacity == 0 ? HASH_TABLE_BUCKET_DEFAULT : (bucket->capacity << 1);
+			bucket->pairs = realloc(bucket->pairs, bucket->capacity * sizeof(*bucket->pairs));
 		}
+		bucket->count++;
+		updated = NULL;
 	} else {
-		struct hash_table_bucket *previous;
-
-		while(bucket->key != key
-			&& bucket->next != NULL) {
-			previous = bucket;
-			bucket = bucket->next;
-		}
-
-		if(bucket->key == key) {
-			previous->next = bucket->next;
-			bucket->next = NULL;
-			hash_table_bucket_destroy(bucket);
-		}
+		updated = current->value;
 	}
+
+	current->value = value;
+	current->key = key;
+
+	return updated;
 }
 
 hash_table_value_t *
-hash_table_find(const struct hash_table *table,
-	hash_table_key_t key) {
-	const struct hash_table_bucket *bucket = table->buckets + key % table->length;
-	hash_table_value_t *value;
+hash_table_access(struct hash_table *table, hash_table_key_t key) {
+	struct hash_table_bucket * const bucket = HASH_TABLE_BUCKETOF(table, key);
+	struct hash_table_pair *current = bucket->pairs,
+		* const end = bucket->pairs + bucket->count;
 
-	while(bucket->key != key
-		&& bucket->next != NULL) {
-		bucket = bucket->next;
+	while(current < end && current->key != key) {
+		current++;
 	}
 
-	if(bucket->key == key) {
-		value = bucket->value;
+	return current == end ? NULL : current->value;
+}
+
+hash_table_value_t *
+hash_table_remove(struct hash_table *table, hash_table_key_t key) {
+	struct hash_table_bucket * const bucket = HASH_TABLE_BUCKETOF(table, key);
+	struct hash_table_pair *current = bucket->pairs,
+		* const end = bucket->pairs + bucket->count;
+	hash_table_value_t *removed;
+
+	while(current < end && current->key != key) {
+		current++;
+	}
+
+	if(current == end) {
+		removed = NULL;
 	} else {
-		value = NULL;
+		removed = current->value;
+		*current = end[-1];
+		bucket->count--;
 	}
 
-	return value;
+	return removed;
 }
 
